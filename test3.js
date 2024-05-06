520ea34d9aa9745af4788e20a25c33841bc25d86
YangChengfan <iuzumakin@gmail.com>
2024-05-06 11:55:39 +0800

    Update test3.js

--- test3.js
+++ test3.js
@@ -3,6 +3,7 @@ var obj = JSON.parse(body);
 
 
 obj['data']['saleRemindVO']['onSaleStatus'] = 0;
+obj['data']['saleRemindVO']['needCountDown'] = false;
 body = JSON.stringify(obj);
 
 console.log(body);
